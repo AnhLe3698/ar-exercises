f38673f7a76893237ae0bf8207fbe32dd16a85f2
AnhLe3698 <anhviet.le00@hotmail.com>
2023-02-04 04:46:30 -0700

    Add new employees

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,13 +6,15 @@ puts "----------"
 
 # Your code goes here ...
 
-# Load the first store (with id = 1) from the database and assign it to an instance 
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+@store1 = Store.find(1)
 
-total_revenue = Store.sum(:annual_revenue)
-puts total_revenue
+# Load the second store from the database and assign it to @store2.
+@store2 = Store.find(2)
 
-average_revenue = Store.average(:annual_revenue)
-puts average_revenue
 
-million_dollar_stores = Store.where("annual_revenue >= 1000000").count
-puts "#{million_dollar_stores} stores are generating $1M or more in annual sales."
+# Update the first store (@store1) instance in the database. (Change its name or something.)
+@store1.name = "New Name for Store 1"
+
+@store1.save
+
